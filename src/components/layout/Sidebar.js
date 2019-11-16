aa140825861cf09d0e38662653441f9c4ef2a5e0
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-11-16 12:37:11 -0600

    Sidebar agregado

---
+++ src/components/layout/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return ( 
+        <aside>
+            <h1>MERN<span>Tasks</span></h1>
+
+            <div className="proyectos">
+                <h2>Tus Proyectos</h2>
+            </div>
+        </aside>
+     );
+}
+ 
+export default Sidebar;
--- src/components/proyectos/Proyectos.js
+++ src/components/proyectos/Proyectos.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import Sidebar from '../layout/Sidebar';
 
 const Proyectos = () => {
     return ( 
-        <h1>Desde Proyectos</h1>
+        <div className="contenedor-app">
+            <Sidebar />
+
+            <div className="seccion-principal">
+                <main>
+
+                    <div className="contenedor-tareas">
+
+                    </div>
+                </main>
+            </div>
+        </div>
      );
 }
  
