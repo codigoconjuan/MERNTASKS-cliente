ac5356c530936b8a723ad76a031c6c32428edfc4
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-11-16 15:10:42 -0600

    state de proyectos desde proyectoState

--- src/components/proyectos/ListadoProyectos.js
+++ src/components/proyectos/ListadoProyectos.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Proyecto from './Proyecto';
+import proyectoContext from '../../context/proyectos/proyectoContext';
 
 const ListadoProyectos = () => {
 
-    const proyectos = [
-        {nombre: 'Tienda Virtual'},
-        {nombre: 'Intranet'},
-        {nombre: 'Diseño de Sitio web'}
-    ]
+    // Extrar proyectos de state inicial
+    const proyectosContext = useContext(proyectoContext);
+    const { proyectos } = proyectosContext;
+
+    // revisar si proyectos tiene contenido
+    if(proyectos.length === 0 ) return null;
 
     return ( 
 
         <ul className="listado-proyectos">
             {proyectos.map(proyecto => (
                 <Proyecto 
+                    key={proyecto.id}
                     proyecto={proyecto}
                 />
             ))}
--- src/context/proyectos/proyectoState.js
+++ src/context/proyectos/proyectoState.js
@@ -7,6 +7,12 @@ import { FORMULARIO_PROYECTO } from '../../types';
 const ProyectoState = props => {
 
     const initialState = {
+        proyectos : [
+            { id: 1, nombre: 'Tienda Virtual' },
+            { id: 2, nombre: 'Intranet' },
+            { id: 3, nombre: 'Diseño de Sitio web' },
+            { id: 4, nombre: 'MERN '}
+        ],
         formulario : false
     }
 
@@ -25,6 +31,7 @@ const ProyectoState = props => {
     return (
         <proyectoContext.Provider
             value={{
+                proyectos: state.proyectos,
                 formulario: state.formulario,
                 mostrarFormulario
             }}
