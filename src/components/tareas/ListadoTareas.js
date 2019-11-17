e1e860297bb4f4130b9a29914c93eb7d54d4ed17
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-11-16 18:42:53 -0600

    Mostrar el proyecto seleccionado

--- src/components/tareas/ListadoTareas.js
+++ src/components/tareas/ListadoTareas.js
@@ -1,7 +1,18 @@
-import React, { Fragment} from 'react';
+import React, { Fragment, useContext} from 'react';
 import Tarea from './Tarea';
+import proyectoContext from '../../context/proyectos/proyectoContext';
 
 const ListadoTareas = () => {
+
+    // Extrar proyectos de state inicial
+    const proyectosContext = useContext(proyectoContext);
+    const { proyecto } = proyectosContext;
+
+    // Si no hay proyecto seleccionado
+    if(!proyecto) return <h2>Selecciona un proyecto</h2>;
+
+    // Array destructuring para extraer el proyecto actual
+    const [proyectoActual] =  proyecto;
 
     const tareasProyecto = [
         { nombre: 'Elegir Plataforma', estado: true},
@@ -12,7 +23,7 @@ const ListadoTareas = () => {
 
     return ( 
         <Fragment>
-            <h2>Proyecto: Tienda Virtual</h2>
+            <h2>Proyecto: {proyectoActual.nombre} </h2>
 
             <ul className="listado-tareas">
                 {tareasProyecto.length === 0 
