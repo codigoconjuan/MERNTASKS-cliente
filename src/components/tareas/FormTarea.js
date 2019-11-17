440b110882103466bc7983a1ba60a1a41448b902
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-11-16 18:48:07 -0600

    Mostrar el formulario de tareas si hay proyecto

--- src/components/tareas/FormTarea.js
+++ src/components/tareas/FormTarea.js
@@ -1,6 +1,19 @@
-import React from 'react';  
+import React, { useContext } from 'react';  
+import proyectoContext from '../../context/proyectos/proyectoContext';
+
 
 const FormTarea = () => {
+
+    // Extrar si un proyecto esta activo
+    const proyectosContext = useContext(proyectoContext);
+    const { proyecto } = proyectosContext;
+
+    // Si no hay proyecto seleccionado
+    if(!proyecto) return null;
+
+    // Array destructuring para extraer el proyecto actual
+    const [proyectoActual] =  proyecto;
+
     return ( 
         <div className="formulario">
             <form>
