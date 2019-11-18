89c42be792ae4490d07f5c258315f3f5b3ff0581
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-11-18 13:37:35 -0600

    Agregando Tareas a tareastate

--- src/components/tareas/ListadoTareas.js
+++ src/components/tareas/ListadoTareas.js
@@ -14,12 +14,7 @@ const ListadoTareas = () => {
     // Array destructuring para extraer el proyecto actual
     const [proyectoActual] =  proyecto;
 
-    const tareasProyecto = [
-        { nombre: 'Elegir Plataforma', estado: true},
-        { nombre: 'Elegir Colores', estado: false},
-        { nombre: 'Elegir Plataformas de pago', estado: false},
-        { nombre: 'Elegir Hosting', estado: true}
-    ];
+    const tareasProyecto = [];
 
     // Elimina un proyecto
     const onClickEliminar = () => {
--- src/context/tareas/tareaState.js
+++ src/context/tareas/tareaState.js
@@ -4,14 +4,32 @@ import TareaReducer from './tareaReducer';
 
 const TareaState = props => {
     const initialState = {
-        tareas: [],
+        tareas: [
+            { nombre: 'Elegir Plataforma', estado: true, proyectoId: 1},
+            { nombre: 'Elegir Colores', estado: false, proyectoId: 2},
+            { nombre: 'Elegir Plataformas de pago', estado: false, proyectoId: 3},
+            { nombre: 'Elegir Hosting', estado: true, proyectoId: 4},
+            { nombre: 'Elegir Plataforma', estado: true, proyectoId: 1},
+            { nombre: 'Elegir Colores', estado: false, proyectoId: 2},
+            { nombre: 'Elegir Plataformas de pago', estado: false, proyectoId: 3},
+            { nombre: 'Elegir Plataforma', estado: true, proyectoId: 4},
+            { nombre: 'Elegir Colores', estado: false, proyectoId: 1},
+            { nombre: 'Elegir Plataformas de pago', estado: false, proyectoId: 2},
+            { nombre: 'Elegir Plataforma', estado: true, proyectoId: 3},
+            { nombre: 'Elegir Colores', estado: false, proyectoId: 4},
+            { nombre: 'Elegir Plataformas de pago', estado: false, proyectoId: 3}
+        ],
     }
 
     // Crear dispatch y state
     const [state, dispatch] = useReducer(TareaReducer, initialState);
 
     return (
-        <TareaContext.Provider>
+        <TareaContext.Provider
+            value={{
+                tareas: state.tareas
+            }}
+        >
             {props.children}
         </TareaContext.Provider>
     )
