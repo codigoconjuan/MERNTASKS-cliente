b838342ce8f0d4bed1cc92b4577cb3d912b7ce8a
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-11-18 17:27:59 -0600

    animaciones a los proyectos

--- src/components/proyectos/ListadoProyectos.js
+++ src/components/proyectos/ListadoProyectos.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from 'react';
 import Proyecto from './Proyecto';
 import proyectoContext from '../../context/proyectos/proyectoContext';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 const ListadoProyectos = () => {
 
@@ -21,12 +22,19 @@ const ListadoProyectos = () => {
     return ( 
 
         <ul className="listado-proyectos">
-            {proyectos.map(proyecto => (
-                <Proyecto 
-                    key={proyecto.id}
-                    proyecto={proyecto}
-                />
-            ))}
+            <TransitionGroup>
+                {proyectos.map(proyecto => (
+                    <CSSTransition
+                        key={proyecto.id}
+                        timeout={200}
+                        classNames="proyecto"
+                    >
+                        <Proyecto 
+                            proyecto={proyecto}
+                        />
+                    </CSSTransition>
+                ))}
+            </TransitionGroup>
         </ul>
      );
 }
