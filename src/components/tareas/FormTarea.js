4c619c7e1daa18c644f7125eef6a52998a4ddd92
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-11-18 18:04:11 -0600

    Modifica formulario de tareas de acuerdo a si hay tarea seleccionada

--- src/components/tareas/FormTarea.js
+++ src/components/tareas/FormTarea.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';  
+import React, { useContext, useState, useEffect } from 'react';  
 import proyectoContext from '../../context/proyectos/proyectoContext';
 import tareaContext from '../../context/tareas/tareaContext';
 
@@ -10,7 +10,18 @@ const FormTarea = () => {
 
     // obtener la función del context de tarea
     const tareasContext = useContext(tareaContext);
-    const { errortarea, agregarTarea, validarTarea, obtenerTareas } = tareasContext;
+    const { tareaseleccionada,  errortarea, agregarTarea, validarTarea, obtenerTareas } = tareasContext;
+
+    // Effect que detecta si hay una tarea seleccionada
+    useEffect(() => {
+        if(tareaseleccionada !== null) {
+            guardarTarea(tareaseleccionada)
+        } else {
+            guardarTarea({
+                nombre: ''
+            })
+        }
+    }, [tareaseleccionada]); 
 
     // State del formulario
     const [tarea, guardarTarea] = useState({
@@ -77,7 +88,7 @@ const FormTarea = () => {
                     <input 
                         type="submit"
                         className="btn btn-primario btn-submit btn-block"
-                        value="Agregar Tarea"
+                        value={tareaseleccionada ? 'Editar Tarea' : 'Agregar Tarea'}
                     />
                 </div>
             </form>
