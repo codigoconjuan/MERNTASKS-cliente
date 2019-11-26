1eadf947dc834350d816e3b52eb06072186b549c
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-11-26 17:28:43 -0600

    finalizando validación

--- src/components/auth/NuevaCuenta.js
+++ src/components/auth/NuevaCuenta.js
@@ -36,11 +36,20 @@ const NuevaCuenta = () => {
             password.trim() === '' || 
             confirmar.trim() === '' ) {
                 mostrarAlerta('Todos los campos son obligatorios', 'alerta-error');
+                return;
             }
 
         // Password minimo de 6 caracteres
+        if(password.length < 6) {
+            mostrarAlerta('El password debe ser de al menos 6 caracteres', 'alerta-error');
+            return;
+        }
 
         // Los 2 passwords son iguales
+        if(password !== confirmar) {
+            mostrarAlerta('Los passwords no son iguales', 'alerta-error');
+            return;
+        }
 
         // Pasarlo al action
 
