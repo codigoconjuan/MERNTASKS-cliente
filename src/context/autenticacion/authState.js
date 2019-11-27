7d638fc0e92f6c6910f0087d895876fa76068dc6
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-11-26 18:28:19 -0600

    cors y registro de usuario

--- src/context/autenticacion/authState.js
+++ src/context/autenticacion/authState.js
@@ -25,17 +25,24 @@ const AuthState = props => {
 
     const registrarUsuario = async datos => {
         try {
+
             const respuesta = await clienteAxios.post('/api/usuarios', datos);
-            console.log(respuesta);
+            console.log(respuesta.data);
 
             dispatch({
-                type: REGISTRO_EXITOSO
+                type: REGISTRO_EXITOSO,
+                payload: respuesta.data
             })
         } catch (error) {
-            console.log(error);
+            // console.log(error.response.data.msg);
+            const alerta = {
+                msg: error.response.data.msg,
+                categoria: 'alerta-error'
+            }
 
             dispatch({
-                type: REGISTRO_ERROR
+                type: REGISTRO_ERROR,
+                payload: alerta
             })
         }
     }
