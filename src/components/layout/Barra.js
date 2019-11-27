da96b5520bcd7b7acf7f4055a702b54ac056d4e0
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-11-27 16:50:02 -0600

    mostrar nombre en la barra superior

--- src/components/layout/Barra.js
+++ src/components/layout/Barra.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
+import AuthContext from '../../context/autenticacion/authContext';
+
 
 const Barra = () => {
+
+    // Extraer la información de autenticación
+    const authContext = useContext(AuthContext);
+    const { usuario, usuarioAutenticado  } = authContext;
+
+    useEffect(() => {
+        usuarioAutenticado();
+    }, []);
+
+
+
     return ( 
         <header className="app-header">
-            <p className="nombre-usuario">Hola <span>Juan Pablo</span> </p>
+            {usuario ? <p className="nombre-usuario">Hola <span>{usuario.nombre} </span> </p> : null}
+            
 
             <nav className="nav-principal">
                 <a href="#!">Cerrar Sesión</a>
