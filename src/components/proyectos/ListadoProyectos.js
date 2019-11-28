bda1c115605358bdfa0db7648bf1eccbda7e4d65
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-11-28 12:27:31 -0600

    obteniendo proyectos de la api

--- src/components/proyectos/ListadoProyectos.js
+++ src/components/proyectos/ListadoProyectos.js
@@ -24,7 +24,7 @@ const ListadoProyectos = () => {
             <TransitionGroup>
                 {proyectos.map(proyecto => (
                     <CSSTransition
-                        key={proyecto.id}
+                        key={proyecto._id}
                         timeout={200}
                         classNames="proyecto"
                     >
--- src/context/proyectos/proyectoState.js
+++ src/context/proyectos/proyectoState.js
@@ -16,13 +16,6 @@ import clienteAxios from '../../config/axios';
 
 const ProyectoState = props => {
 
-    const proyectos = [
-        { id: 1, nombre: 'Tienda Virtual' },
-        { id: 2, nombre: 'Intranet' },
-        { id: 3, nombre: 'Diseño de Sitio web' },
-        { id: 4, nombre: 'MERN '}
-    ]
-
     const initialState = {
         proyectos : [],
         formulario : false,
@@ -41,11 +34,17 @@ const ProyectoState = props => {
     }
 
     // Obtener los proyectos
-    const obtenerProyectos = () => {
-        dispatch({
-            type: OBTENER_PROYECTOS,
-            payload: proyectos
-        })
+    const obtenerProyectos = async () => {
+        try {
+            const resultado = await clienteAxios.get('/api/proyectos');
+
+            dispatch({
+                type: OBTENER_PROYECTOS,
+                payload: resultado.data.proyectos
+            })
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     // Agregar nuevo proyecto
